a7482b5b0a02ec545e42f295c4684b1f6b4cffa2
Artur Mleczko <72202741+arturmleczko@users.noreply.github.com>
2021-04-15 11:59:34 +0200

    Fully finished application

--- Temat 4 - Metoda fabrykująca i enum/src/ts/game.creator.ts
+++ Temat 4 - Metoda fabrykująca i enum/src/ts/game.creator.ts
@@ -1,8 +1,8 @@
 import { Game } from './game.model';
 
-import { TicTacToe } from '../games/ticTacToe/TicTacToe';
-import { BattleShips } from '../games/battleShips/BattleShips';
-import { SpaceShips } from '../games/spaceShips/SpaceShips';
+import { TicTacToe } from './games/ticTacToe/TicTacToe';
+import { BattleShips } from './games/battleShips/BattleShips';
+import { SpaceShips } from './games/spaceShips/SpaceShips';
 
 export interface GameCreator {
 	getGame(): Game;
---
+++ Temat 4 - Metoda fabrykująca i enum/src/ts/games/battleShips/BattleShips.ts
@@ -0,0 +1,17 @@
+import { Game } from '../../game.model';
+
+export class BattleShips implements Game {
+	name: string;
+
+	constructor() {
+		this.name = 'Battle Ships';
+	}
+
+	getGameElement(): HTMLElement {
+		const div = document.createElement('div');
+		div.appendChild(document.createTextNode('Hello BattleShips'));
+		return div;
+	}
+
+	getGameLogic() {}
+}
